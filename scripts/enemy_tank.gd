f3964dd0b077c4a17ad5d12e94578b4710af596f
RaduMarin19 <marinradul33t@gmail.com>
2024-12-24 12:01:22 +0200

    Added map

--- scripts/enemy_tank.gd
+++ scripts/enemy_tank.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_to_group("enemies")
 	HealthBar.value=100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/tank.gd
+++ scripts/tank.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 120.0
-const TURRET_SPEED=60.0
+const SPEED = 300.0
+const TURRET_SPEED=100.0
 var player_state
 var can_shoot=true
 
